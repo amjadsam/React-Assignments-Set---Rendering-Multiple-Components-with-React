85c20fffc798a758878de38fdbd375c071868c2d
amjadsam <68984249+amjadsam@users.noreply.github.com>
2021-01-15 00:06:37 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+<p data-ns-test="project-name">Food Delivery App</p>
+<p data-ns-test="project-description">Just like swiggy , zomatoe</p>
+    </div>
   )
 }
 
